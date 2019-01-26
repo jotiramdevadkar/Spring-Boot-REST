b65e88211c12199128b8950edcec0fff9b254709
remove <Sandip@Sandip-PC>
2019-01-26 12:21:28 +0530

    Added WebSecurityConfig

---
+++ XcaliberFoodStall/src/main/java/com/jotiram/foodstall/configuration/BeanConfiguration.java
@@ -0,0 +1,90 @@
+package com.jotiram.foodstall.configuration;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableTransactionManagement
+@EnableWebMvc
+public class BeanConfiguration extends WebMvcConfigurerAdapter {
+	
+	@Bean(name="viewResolver")
+	public InternalResourceViewResolver getViewResolver()
+	{
+		InternalResourceViewResolver viewResolver=new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/jsp/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry)
+	{
+		registry.addResourceHandler("/resources/**").addResourceLocations("/WEB-INF/resources/");
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/caliber");
+		dataSource.setUsername("root");
+		dataSource.setPassword("root");
+
+		return dataSource;
+	}
+	
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate()
+	{
+		JdbcTemplate jdbcTemplate =new JdbcTemplate();
+		jdbcTemplate.setDataSource(dataSource());
+		return jdbcTemplate;
+	}
+	
+	@Bean
+	public LocalSessionFactoryBean sessionFactory()
+	{
+		LocalSessionFactoryBean sessionFactory= new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource());
+		sessionFactory.setPackagesToScan("com");
+		
+		Properties hibernateProperties =new Properties();
+		hibernateProperties.getProperty("spring.jpa.hibernate.ddl-auto");
+		hibernateProperties.getProperty("spring.jpa.show-sql");
+		hibernateProperties.getProperty("spring.jpa.properties.hibernate.dialet");
+		
+		hibernateProperties.put("show_sql", true);
+		
+		hibernateProperties.put("spring.jpa.properties.hibernate.id.new_generator_mappings", false);
+		
+		sessionFactory.setHibernateProperties(hibernateProperties);
+		
+		return sessionFactory;
+	}
+	
+	@Bean
+	public HibernateTransactionManager transactionManager()
+	{
+		HibernateTransactionManager txManager=new HibernateTransactionManager();
+		txManager.setSessionFactory(sessionFactory().getObject());
+		
+		return txManager;
+	}
+	
+}
