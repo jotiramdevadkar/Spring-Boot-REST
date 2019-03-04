02ab5afc8c20d800e55a28193aa78f11a6854f55
jotiramdevadkar <jotiamdevadkar@gmail.com>
2019-03-04 11:06:45 +0530

    userDao changes

--- spring-boot-oauth2/src/main/java/com/jotiram/dao/UserDao.java
+++ spring-boot-oauth2/src/main/java/com/jotiram/dao/UserDao.java
@@ -1,9 +1,10 @@
 package com.jotiram.dao;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jotiram.model.User;
-
+@Repository
 public interface UserDao extends CrudRepository<User, Long>{
 	User findByUsername(String username);
 }
